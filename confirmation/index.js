092155dfda5a7e671b7abc44c438e387e55b4f1c
Newton Munene <newtonmunene99@gmail.com>
2019-04-18 16:24:29 +0300

    clean up

--- confirmation/index.js
+++ confirmation/index.js
@@ -6,23 +6,13 @@ const app = express();
 // add some security-related headers to the response
 app.use(helmet());
 app.use(express.json());
-
-app.get("*", (req, res) => {
-  // res.set("Content-Type", "text/html");
-
-  res.send(req.body);
-  console.log(req.body);
-  console.log(req.params);
-  console.log(req.query);
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.post("*", (req, res) => {
   // res.set("Content-Type", "text/html");
 
   res.send(req.body);
   console.log(req.body);
-  console.log(req.params);
-  console.log(req.query);
 });
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const express = require('express')
-const helmet = require('helmet')
+const express = require("express");
+const helmet = require("helmet");
 
-const app = express()
+const app = express();
 
 // add some security-related headers to the response
-app.use(helmet())
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
-    res.set('Content-Type', 'text/html')
-    res.status(200).send(`
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
-        <h2>Go to <a href="/about">/about</a></h2>
-    `)
-})
+app.get("*", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.status(200).send(`
+        <h1>Safaricom Daraja API Endpoint</h1>
+    `);
+});
 
-module.exports = app
+app.get("*", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.status(200).send(`
+        <h1>Safaricom Daraja API Endpoint</h1>
+    `);
+});
+
+module.exports = app;
